2dcda934a270ffc26a352c3e14e853adb4fcb5a3
Reading-magic <514677022@qq.com>
2016-11-07 16:07:08 +0800

    Front page HTML now gennerated from a template

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,10 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVistiorTest(unittest.TestCase):
     def setUp(self):       
         self.browser = webdriver.Firefox(executable_path = 'F:\Mozilla Firefox\geckodriver.exe')
-
+        self.browser.implicitly_wait(3)
     def tearDown(self):
         self.browser.quit()
 
@@ -13,6 +14,25 @@ class NewVistiorTest(unittest.TestCase):
 
         #assert 'Django' in browser.title
         self.assertIn('To-Do',self.browser.title)
+        header_text=self.browser.find_element_by_tag_name('h1').text
+
+        inputbox=self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+        	inputbox.get_attribute('placeholder'),
+        	'Enter a to-do item'
+        	)
+
+        inputbox.send_keys('Buy peacock feathers')
+
+        inputbox.send_keys(Keys.ENTER)
+
+        table=self.browser.find_element_by_id('id_list_table')
+        rows=table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+        	any(row.text=='1:Buy peacock feathers' for row in rows),
+        	"New to-do item did not appear in table"
+        	)
+
         self.fail('Finissh the test!')
 
 
