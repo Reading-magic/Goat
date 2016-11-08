63149c12a689371b730ef17e7fd7bff583ad0621
Reading-magic <514677022@qq.com>
2016-11-08 20:28:17 +0800

    有毒！

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -27,13 +27,18 @@ class NewVistiorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         table=self.browser.find_element_by_id('id_list_table')
-        rows=table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-        	any(row.text=='1:Buy peacock feathers' for row in rows),
-        	"New to-do item did not appear in table"
-        	)
-
-        self.fail('Finissh the test!')
+        rows=table.find_element_by_tag_name('tr')
+       # self.assertTrue(
+        #	any(row.text=='1:Buy peacock feathers' for row in rows),
+        #	"New to-do item did not appear in table -- its text was:\n%s" %(
+         #       table.text),           
+        #	)
+        self.assertIn('1:Buy peacock feathers',rows.text)
+        #self.assertIn(
+         #   '2:Use peacock feathers to make a fly',
+         #   [row.text for row in rows]
+         #   )
+        self.fail('Finish the test!')
 
 
 if __name__ == '__main__':
