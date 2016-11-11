257e5c7937f17e8bbbca94996fcafa0fb02ce729
Reading-magic <514677022@qq.com>
2016-11-11 17:19:28 +0800

    make functional_tests an app,use LiveServerCase

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,19 +1,23 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
 
-class NewVistiorTest(unittest.TestCase):
+class NewVistiorTest(LiveServerTestCase):
     def setUp(self):       
         self.browser = webdriver.Firefox(executable_path = 'F:\Mozilla Firefox\geckodriver.exe')
         self.browser.implicitly_wait(3)
 
+    #def tearDown(self):
+    #	self.browser.quit()
+
     def check_for_row_in_list_table(self,row_text):
         table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
+        rows = table.find_elements_by_tag_name("tr")
         self.assertIn(row_text,[row.text for row in rows])
 
+
     def test_can_start_a_list_and_retrieve_it_later(self):    
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
         header_text=self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do',self.browser.title)
 
@@ -21,8 +25,8 @@ class NewVistiorTest(unittest.TestCase):
         inputbox=self.browser.find_element_by_id('id_new_item')
 
         inputbox.send_keys('Buy peacock feathers')
-      #  inputbox.send_keys(Keys.ENTER) 
-
+        inputbox.submit()#提交表单
+        self.browser.refresh() #刷新
         self.check_for_row_in_list_table('1:Buy peacock feathers')
 
         inputbox=self.browser.find_element_by_id('id_new_item')
@@ -32,7 +36,3 @@ class NewVistiorTest(unittest.TestCase):
         self.check_for_row_in_list_table('1:Buy peacock feathers')
         self.check_for_row_in_list_table('2:Use peacock feathers to make a fly')
         self.fail('Finish the test!')
-
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
