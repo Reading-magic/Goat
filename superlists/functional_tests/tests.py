213f46d0dde97cf8512d731b7c84e277069757c4
[Reading-magic] <[514677022@qq.com]>
2017-02-25 19:37:35 +0800

    change Bootstrap for 3.1.0

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -97,13 +97,14 @@ class NewVistiorTest(StaticLiveServerTestCase):
 		#艾访问首页
         self.browser.get(self.live_server_url)
         self.browser.set_window_size(1024,768)
+        
 
 		#她看到输入框完美地居中显示
         inputbox=self.browser.find_element_by_id('id_new_item')
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
             512,
-            delta=5
+            delta=10
             )
         #她新建一个清单，看到输入框仍完美地居中显示
         inputbox.send_keys('testing\n')
@@ -111,5 +112,5 @@ class NewVistiorTest(StaticLiveServerTestCase):
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
             512,
-            delta=5
+            delta=10
             )
